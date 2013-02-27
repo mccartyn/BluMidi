ba8336508f6a1f57f20a7da732a6d799d00a5e14
Nick McCarty <mccartyn@gmail.com>
2013-02-26 19:16:40 -0800

    Modified Directory Structure added BlueGiga
    Moved the Xcode into a folder named Xcode. Added Bluegiga BLE module
    documentation and sample code.

---
+++ Xcode/BluMidiTests/BluMidiTests.h
@@ -0,0 +1,13 @@
+//
+//  BluMidiTests.h
+//  BluMidiTests
+//
+//  Created by Nick McCarty on 1/26/13.
+//  Copyright (c) 2013 Nick McCarty. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BluMidiTests : SenTestCase
+
+@end
